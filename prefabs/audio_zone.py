0945b6b103df8eadac4bb31c69041aa9397bf5b3
Manuel Martin <manmartin@mozilla.com>
2022-03-29 12:30:37 +0200

    Prefab renames

--- prefabs/audio_zone.py
+++ prefabs/audio_zone.py
@@ -2,8 +2,8 @@ import bpy
 from ..gizmos.gizmo_group import update_gizmos
 
 
-class OBJECT_OT_hba_audio_zone_add(bpy.types.Operator):
-    bl_idname = "object.hba_audio_zone_add"
+class OBJECT_OT_hba_prefab_audio_zone_add(bpy.types.Operator):
+    bl_idname = "object.hba_prefab_audio_zone_add"
     bl_label = "Add Audio Zone"
     bl_options = {"UNDO"}
 
@@ -16,8 +16,8 @@ class OBJECT_OT_hba_audio_zone_add(bpy.types.Operator):
         return {"FINISHED"}
 
 
-class RENDER_PT_hba_audio_zone(bpy.types.Panel):
-    bl_idname = "HBA_PT_Audio_Zone"
+class RENDER_PT_hba_prefab_audio_zone(bpy.types.Panel):
+    bl_idname = "HBA_PT_Prefab_Audio_Zone"
     bl_label = "Audio Zone"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
@@ -33,23 +33,23 @@ class RENDER_PT_hba_audio_zone(bpy.types.Panel):
 
         layout = self.layout
         row = layout.row()
-        row.prop(obj, "HBA_audio_zone_prop_1")
+        row.prop(obj, "HBA_prefab_audio_zone_prop_1")
 
 
 def register():
-    bpy.types.Object.HBA_audio_zone_prop_1 = bpy.props.BoolProperty(
+    bpy.types.Object.HBA_prefab_audio_zone_prop_1 = bpy.props.BoolProperty(
         name="Prop 1",
         description="Prop 1",
         default=False
     )
-    bpy.utils.register_class(OBJECT_OT_hba_audio_zone_add)
-    bpy.utils.register_class(RENDER_PT_hba_audio_zone)
+    bpy.utils.register_class(OBJECT_OT_hba_prefab_audio_zone_add)
+    bpy.utils.register_class(RENDER_PT_hba_prefab_audio_zone)
 
 
 def unregister():
-    del bpy.types.Object.HBA_audio_zone_prop_1
-    bpy.utils.unregister_class(RENDER_PT_hba_audio_zone)
-    bpy.utils.unregister_class(OBJECT_OT_hba_audio_zone_add)
+    del bpy.types.Object.HBA_prefab_audio_zone_prop_1
+    bpy.utils.unregister_class(RENDER_PT_hba_prefab_audio_zone)
+    bpy.utils.unregister_class(OBJECT_OT_hba_prefab_audio_zone_add)
 
 
 if __name__ == "__main__":
