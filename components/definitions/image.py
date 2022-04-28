a45ca7e3a7d456dbea083f719ae0ace05c311f97
Manuel Martin <manmartin@mozilla.com>
2022-04-28 17:29:18 +0200

    audio, video and personal-space-invader components

--- components/definitions/image.py
+++ components/definitions/image.py
@@ -1,18 +1,7 @@
 from bpy.props import EnumProperty, StringProperty, BoolProperty
 from .hubs_component import HubsComponent
 from ..types import Category, PanelType, NodeType
-
-# TODO Add this component in the scene by default?
-
-TRANSPARENCY_MODE = [("opaque", "No transparency (opaque)", "Alpha channel will be ignored"),
-                     ("blend", "Gradual transparency (blend)",
-                      "Alpha channel will be applied"),
-                     ("mask", "Binary transparency (mask)",
-                      "Alpha channel will be used as a threshold between opaque and transparent pixels")]
-
-PROJECTION_MODE = [("flat", "2D image (flat)", "Image will be shown on a 2D surface"),
-                   ("360-equirectangular",
-                    "Spherical (360-equirectangular)", "Image will be shown on a sphere")]
+from ..consts import PROJECTION_MODE, TRANSPARENCY_MODE
 
 
 class Image(HubsComponent):
@@ -23,7 +12,7 @@ class Image(HubsComponent):
         'category': Category.ELEMENTS,
         'node_type': NodeType.NODE,
         'panel_type': PanelType.OBJECT,
-        'icon': 'image.png',
+        'icon': 'FILE_IMAGE',
         'deps': ['networked']
     }
 
---
+++ components/definitions/personal_space_invader.py
@@ -0,0 +1,28 @@
+import bpy
+from bpy.props import BoolProperty, FloatProperty
+from .hubs_component import HubsComponent
+from ..types import Category, PanelType, NodeType
+
+
+class AmbientLight(HubsComponent):
+    _definition = {
+        'id': 'personal-space-invader',
+        'name': 'hubs_component_personal_space_invader',
+        'display_name': 'Personal Space Invader',
+        'category': Category.AVATAR,
+        'node_type': NodeType.NODE,
+        'panel_type': PanelType.OBJECT,
+        'icon': 'LIGHT_HEMI'
+    }
+
+    radius: FloatProperty(name="Radius",
+                          description="Radius",
+                          default=0.1)
+
+    useMaterial: BoolProperty(name="Use Material",
+                              description="Use Material",
+                              default=False)
+
+    invadingOpacity: FloatProperty(name="Invading Opacity",
+                                   description="Invading Opacity",
+                                   default=0.3)
