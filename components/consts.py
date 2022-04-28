b2d34895c5b4cfa05984df1fa01174a5aeaf2a00
Manuel Martin <manmartin@mozilla.com>
2022-04-28 17:29:18 +0200

    audio, video and personal-space-invader components

--- components/consts.py
+++ components/consts.py
@@ -9,3 +9,13 @@ DISTACE_MODELS = [("inverse", "Inverse drop off (inverse)",
                   "Volume will decrease expoentially with distance")]
 
 MAX_ANGLE = radians(360.0)
+
+PROJECTION_MODE = [("flat", "2D image (flat)", "Image will be shown on a 2D surface"),
+                   ("360-equirectangular",
+                    "Spherical (360-equirectangular)", "Image will be shown on a sphere")]
+
+TRANSPARENCY_MODE = [("opaque", "No transparency (opaque)", "Alpha channel will be ignored"),
+                     ("blend", "Gradual transparency (blend)",
+                      "Alpha channel will be applied"),
+                     ("mask", "Binary transparency (mask)",
+                      "Alpha channel will be used as a threshold between opaque and transparent pixels")]
