be047d00a011065ff6be323456038e73e4d63374
Manuel Martin <manmartin@mozilla.com>
2022-04-29 11:14:02 +0200

    billboard, scale-audio-feedback and spawner

---
+++ components/definitions/billboard.py
@@ -0,0 +1,17 @@
+from bpy.props import BoolProperty
+from .hubs_component import HubsComponent
+from ..types import Category, PanelType, NodeType
+
+
+class Billboard(HubsComponent):
+    _definition = {
+        'id': 'billboard',
+        'name': 'hubs_component_billboard',
+        'display_name': 'Billboard',
+        'category': Category.ELEMENTS,
+        'node_type': NodeType.NODE,
+        'panel_type': PanelType.OBJECT
+    }
+
+    onlyY: BoolProperty(
+        name="Only Y", description="Rotate only in Y axis", default=False)
