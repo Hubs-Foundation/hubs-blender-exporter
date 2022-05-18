d8b10d9ec0819c22c08541ebe08a09b41d618a34
Manuel Martin <manmartin@mozilla.com>
2022-05-18 15:38:02 +0200

    Support multiple actions in loop animation component

--- addons/io_hubs_addon/components/definitions/loop_animation.py
+++ addons/io_hubs_addon/components/definitions/loop_animation.py
@@ -1,32 +1,110 @@
 import atexit
 import bpy
-from bpy.props import BoolProperty, StringProperty, EnumProperty
+from bpy.props import StringProperty, CollectionProperty, IntProperty, BoolProperty
+from bpy.types import PropertyGroup, Menu, Operator
 from bpy.types import PropertyGroup
 from .hubs_component import HubsComponent
 from ..types import Category, PanelType, NodeType
 
 
-class ClipPropertyType(PropertyGroup):
+class ActionsList(bpy.types.UIList):
+    bl_idname = "HUBS_UL_ACTIONS_list"
+
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+        key_block = item
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            split = layout.split(factor=0.90, align=False)
+            split.prop(key_block, "name", text="",
+                       emboss=False, icon_value=icon)
+            row = split.row(align=True)
+            row.emboss = 'NONE_OR_STATUS'
+        elif self.layout_type == 'GRID':
+            layout.alignment = 'CENTER'
+            layout.label(text="", icon_value=icon)
+
+
+class AddActionOperator(Operator):
+    bl_idname = "hubs_loop_animation.add_action"
+    bl_label = "Add Action"
+
+    action_name: StringProperty(
+        name="Action Name", description="Action Name", default="")
+
+    def execute(self, context):
+        ob = context.object
+        action = ob.hubs_component_loop_animation.actions_list.add()
+        action.name = self.action_name
+
+        ob.hubs_component_loop_animation.name = ",".join(
+            ob.hubs_component_loop_animation.actions_list.keys())
+
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        return self.execute(context)
+
+
+class RemoveActionOperator(Operator):
+    bl_idname = "hubs_loop_animation.remove_action"
+    bl_label = "Remove Action"
+
+    @classmethod
+    def poll(self, context):
+        return context.object.hubs_component_loop_animation.active_action_key != -1
+
+    def execute(self, context):
+        ob = context.object
+
+        active_action_key = ob.hubs_component_loop_animation.active_action_key
+        ob.hubs_component_loop_animation.actions_list.remove(
+            active_action_key)
+
+        ob.hubs_component_loop_animation.name = ",".join(
+            ob.hubs_component_loop_animation.actions_list.keys())
+
+        return {'FINISHED'}
+
+
+def has_action(actions_list, action):
+    exists = False
+    for item in actions_list:
+        if item.name == action:
+            exists = True
+            break
+
+    return exists
+
+
+class ActionsContextMenu(Menu):
+    bl_idname = "HUBS_MT_ACTIONS_context_menu"
+    bl_label = "Actions Specials"
+
+    def draw(self, context):
+        no_actions = True
+        for a in bpy.data.actions:
+            if not has_action(context.object.hubs_component_loop_animation.actions_list, a):
+                self.layout.operator(AddActionOperator.bl_idname, icon='OBJECT_DATA',
+                                     text=a.name).action_name = a.name
+                no_actions = False
+
+        if no_actions:
+            self.layout.label(text="No actions found")
+
+
+class ActionPropertyType(PropertyGroup):
     name: StringProperty(
-        name="Animation name",
-        description="Animation name",
+        name="Action name",
+        description="Action Name",
         default=""
     )
 
 
-bpy.utils.register_class(ClipPropertyType)
+bpy.utils.register_class(ActionPropertyType)
 
 
 @atexit.register
 def unregister():
-    bpy.utils.unregister_class(ClipPropertyType)
-
-
-def get_clips(self, context):
-    clips = []
-    for a in bpy.data.actions:
-        clips.append((a.name, a.name, ""))
-    return clips
+    bpy.utils.unregister_class(ActionPropertyType)
 
 
 class LoopAnimation(HubsComponent):
@@ -40,11 +118,20 @@ class LoopAnimation(HubsComponent):
         'icon': 'LOOP_BACK'
     }
 
-    clip: EnumProperty(
+    actions_list: CollectionProperty(
+        type=ActionPropertyType,
+        options={'HIDDEN', 'SKIP_SAVE'})
+
+    clip: StringProperty(
         name="Animation Clip",
         description="Animation clip to use",
-        items=get_clips,
-        options={'HIDDEN', 'SKIP_SAVE'}
+        default=""
+    )
+
+    active_action_key: IntProperty(
+        name="Active action index",
+        description="Active action index",
+        default=-1
     )
 
     paused: BoolProperty(
@@ -54,12 +141,38 @@ class LoopAnimation(HubsComponent):
     )
 
     def draw(self, context, layout):
-        if len(bpy.data.actions) > 0:
-            super().draw(context, layout)
-        else:
-            layout.label(text='No clips available',
-                         icon='ERROR')
+        layout.label(text='Animations to play:')
 
-    @classmethod
-    def poll(cls, context):
-        return context.object.type == 'ARMATURE'
+        row = layout.row()
+        row.template_list(ActionsList.bl_idname, "", self,
+                          "actions_list", self, "active_action_key", rows=3)
+
+        col = row.column(align=True)
+
+        col.menu(ActionsContextMenu.bl_idname, icon='ADD', text="")
+        col.operator(RemoveActionOperator.bl_idname,
+                     icon='REMOVE', text="")
+
+        layout.separator()
+
+        layout.prop(data=self, property='paused')
+
+    def gather(self, export_settings, object):
+        return {
+            'clip': self.clip,
+            'paused': self.paused
+        }
+
+    @staticmethod
+    def register():
+        bpy.utils.register_class(ActionsList)
+        bpy.utils.register_class(ActionsContextMenu)
+        bpy.utils.register_class(AddActionOperator)
+        bpy.utils.register_class(RemoveActionOperator)
+
+    @staticmethod
+    def unregister():
+        bpy.utils.unregister_class(ActionsList)
+        bpy.utils.unregister_class(ActionsContextMenu)
+        bpy.utils.unregister_class(AddActionOperator)
+        bpy.utils.unregister_class(RemoveActionOperator)
