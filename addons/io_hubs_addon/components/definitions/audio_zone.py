a6502020be27774de7f38902a7bbfecbc57e2816
Manuel Martin <manmartin@mozilla.com>
2022-05-25 11:15:13 +0200

    Sync audio zone size with object dimensions

--- addons/io_hubs_addon/components/definitions/audio_zone.py
+++ addons/io_hubs_addon/components/definitions/audio_zone.py
@@ -1,9 +1,18 @@
 from bpy.props import BoolProperty
 from bpy.types import Node
+from ..gizmos import CustomModelGizmo
+from ..models import box
 from ..hubs_component import HubsComponent
 from ..types import Category, PanelType
 from mathutils import Matrix
 from .networked import migrate_networked
+
+
+def get_gizmo_scale(ob):
+    if ob.type == 'MESH':
+        return ob.dimensions.copy() / 2
+    else:
+        return ob.scale.copy() * ob.empty_display_size
 
 
 class AudioZone(HubsComponent):
@@ -26,22 +35,31 @@ class AudioZone(HubsComponent):
                         default=True)
 
     @classmethod
-    def create_gizmo(cls, obj, gizmo_group):
-        widget = gizmo_group.gizmos.new('GIZMO_GT_cage_3d')
-        widget.draw_style = ('BOX')
-        loc, rot, _ = obj.matrix_world.decompose()
-        widget.matrix_basis = Matrix.LocRotScale(loc, rot, obj.dimensions)
+    def update_gizmo(cls, ob, gizmo):
+        loc, rot, _ = ob.matrix_world.decompose()
+        mat_out = Matrix.LocRotScale(loc, rot, get_gizmo_scale(ob))
+        gizmo.matrix_basis = mat_out
+        gizmo.hide = not ob.visible_get()
+
+    @classmethod
+    def create_gizmo(cls, ob, gizmo_group):
+        widget = gizmo_group.gizmos.new(CustomModelGizmo.bl_idname)
+        setattr(widget, "hubs_gizmo_shape", box.SHAPE)
+        widget.setup()
+        loc, rot, _ = ob.matrix_world.decompose()
+        widget.matrix_basis = Matrix.LocRotScale(loc, rot, get_gizmo_scale(ob))
+        widget.use_draw_scale = False
+        widget.use_draw_modal = True
         widget.line_width = 3
         widget.color = (0.0, 0.8, 0.0)
-        widget.alpha = 0.5
-        widget.hide = not obj.visible_get()
-        widget.hide_select = True
+        widget.alpha = 1.0
+        widget.hide = not ob.visible_get()
+        widget.hide_select = False
         widget.scale_basis = 1.0
-        widget.use_draw_modal = True
         widget.color_highlight = (0.0, 0.8, 0.0)
-        widget.alpha_highlight = 1.0
+        widget.alpha_highlight = 0.5
 
-        op = widget.target_set_operator("transform.translate")
+        op = widget.target_set_operator("transform.resize")
         op.constraint_axis = False, False, False
         op.orient_type = 'LOCAL'
         op.release_confirm = True
