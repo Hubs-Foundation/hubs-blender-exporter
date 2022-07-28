2013a46b6da38cc8ce4933ce9c48c895d9c680bc
Manuel Martin <manmartin@mozilla.com>
2022-07-28 15:42:43 +0200

    Remove debug print

--- addons/io_hubs_addon/components/definitions/audio_target.py
+++ addons/io_hubs_addon/components/definitions/audio_target.py
@@ -171,5 +171,4 @@ class AudioTarget(HubsComponent):
                         import_settings.vnodes[property_value['index']].blender_object)
 
             else:
-                print(f"{property_name} = {property_value}")
                 setattr(blender_component, property_name, property_value)
