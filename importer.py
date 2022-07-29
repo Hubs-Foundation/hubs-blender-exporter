74743ce6ef1d346444bce93fd8a386c58f578309
Manuel Martin <manmartin@mozilla.com>
2022-07-29 15:12:22 +0200

    Support for importing components in bones

--- importer.py
+++ importer.py
@@ -8,26 +8,26 @@ from .utils import HUBS_CONFIG
 
 EXTENSION_NAME = HUBS_CONFIG["gltfExtensionName"]
 
+armatures = []
 
-def add_hubs_components(gltf2_object, blender_object, import_settings):
-    if not gltf2_object.extensions or EXTENSION_NAME not in gltf2_object.extensions:
-        return
 
-    components_data = gltf2_object.extensions[EXTENSION_NAME]
-    from ..components.components_registry import get_component_by_name
-    for component_name in components_data.keys():
-        component_class = get_component_by_name(component_name)
-        if component_class:
-            component_value = components_data[component_name]
-            component_class.gather_import(
-                import_settings, blender_object, component_name, component_value)
-        else:
-            print('Could not import unsupported component "%s"' %
-                  (component_name))
+def add_hubs_components(gltf_node, blender_object, import_settings):
+    if gltf_node and gltf_node.extensions and EXTENSION_NAME in gltf_node.extensions:
+        components_data = gltf_node.extensions[EXTENSION_NAME]
+        from ..components.components_registry import get_component_by_name
+        for component_name in components_data.keys():
+            component_class = get_component_by_name(component_name)
+            if component_class:
+                component_value = components_data[component_name]
+                component_class.gather_import(
+                    import_settings, blender_object, component_name, component_value)
+            else:
+                print('Could not import unsupported component "%s"' %
+                      (component_name))
 
 
 def add_lightmap(gltf_material, blender_mat, import_settings):
-    if gltf_material.extensions and 'MOZ_lightmap' in gltf_material.extensions:
+    if gltf_material and gltf_material.extensions and 'MOZ_lightmap' in gltf_material.extensions:
         extension = gltf_material.extensions['MOZ_lightmap']
 
         index = extension['index']
@@ -52,6 +52,17 @@ def add_lightmap(gltf_material, blender_mat, import_settings):
             node_tex.outputs["Color"], lightmap_node.inputs["Lightmap"])
 
 
+def add_bones(import_settings):
+    # Bones are created after the armatures so we need to wait until all nodes have been processed to be able to access the bones objects
+    global armatures
+    for blender_object in armatures:
+        for bone in blender_object.data.bones:
+            gltf_bone = next((
+                gltf_node for gltf_node in import_settings.data.nodes if gltf_node.name == bone.name), None)
+            add_hubs_components(
+                gltf_bone, bone, import_settings)
+
+
 class glTF2ImportUserExtension:
 
     def __init__(self):
@@ -62,6 +73,9 @@ class glTF2ImportUserExtension:
     def gather_import_scene_before_hook(self, gltf_scene, blender_scene, import_settings):
         if not self.properties.enabled:
             return
+
+        global armatures
+        armatures.clear()
 
         if import_settings.data.asset and import_settings.data.asset.extras:
             if 'gltf_yup' in import_settings.data.asset.extras:
@@ -74,12 +88,22 @@ class glTF2ImportUserExtension:
 
         self.add_hubs_components(gltf_scene, blender_scene, import_settings)
 
+        # Bones are created after the armatures so we need to wait until all nodes have been processed to be able to access the bones objects
+        add_bones(import_settings)
+        armatures.clear()
+
     def gather_import_node_after_hook(self, vnode, gltf_node, blender_object, import_settings):
         if not self.properties.enabled:
             return
 
         self.add_hubs_components(
             gltf_node, blender_object, import_settings)
+
+        # Node hooks are not called for bones. Bones are created together with their armature.
+        # Unfortunately the bones are created after this hook is called so we need to wait until all nodes have been created.
+        if vnode.is_arma:
+            global armatures
+            armatures.append(blender_object)
 
     def gather_import_image_after_hook(self, gltf_img, blender_image, import_settings):
         # As of Blender 3.2.0 the importer doesn't import images that are not referenced by a material socket.
@@ -100,8 +124,8 @@ class glTF2ImportUserExtension:
 
         add_lightmap(gltf_material, blender_mat, import_settings)
 
-    def add_hubs_components(self, gltf2_object, blender_object, import_settings):
-        add_hubs_components(gltf2_object, blender_object, import_settings)
+    def add_hubs_components(self, gltf_node, blender_object, import_settings):
+        add_hubs_components(gltf_node, blender_object, import_settings)
 
 
 # import hooks were only recently added to the glTF exporter, so make a custom hook for now
@@ -110,7 +134,7 @@ orig_BlenderMaterial_create = BlenderMaterial.create
 orig_BlenderScene_create = BlenderScene.create
 
 
-@staticmethod
+@ staticmethod
 def patched_BlenderNode_create_object(gltf, vnode_id):
     blender_object = orig_BlenderNode_create_object(gltf, vnode_id)
 
@@ -128,10 +152,16 @@ def patched_BlenderNode_create_object(gltf, vnode_id):
 
     add_hubs_components(node, vnode.blender_object, gltf)
 
+    # Node hooks are not called for bones. Bones are created together with their armature.
+    # Unfortunately the bones are created after this hook is called so we need to wait until all nodes have been created.
+    if vnode.is_arma:
+        global armatures
+        armatures.append(vnode.blender_object)
+
     return blender_object
 
 
-@staticmethod
+@ staticmethod
 def patched_BlenderMaterial_create(gltf, material_idx, vertex_color):
     orig_BlenderMaterial_create(
         gltf, material_idx, vertex_color)
@@ -142,12 +172,19 @@ def patched_BlenderMaterial_create(gltf, material_idx, vertex_color):
     add_lightmap(gltf_material, blender_mat, gltf)
 
 
-@staticmethod
+@ staticmethod
 def patched_BlenderScene_create(gltf):
+    global armatures
+    armatures.clear()
+
     orig_BlenderScene_create(gltf)
     gltf_scene = gltf.data.scenes[gltf.data.scene]
     blender_object = bpy.data.scenes[gltf.blender_scene]
     add_hubs_components(gltf_scene, blender_object, gltf)
+
+    # Bones are created after the armatures so we need to wait until all nodes have been processed to be able to access the bones objects
+    add_bones(gltf)
+    armatures.clear()
 
 
 def register():
