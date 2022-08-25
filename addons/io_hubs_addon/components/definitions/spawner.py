f3a5f8136d3a098e59ba3e8701868cce1523b388
Manuel Martin <manmartin@mozilla.com>
2022-08-25 13:50:43 +0200

    Support for Spoke GLB import

--- addons/io_hubs_addon/components/definitions/spawner.py
+++ addons/io_hubs_addon/components/definitions/spawner.py
@@ -2,6 +2,7 @@ import bpy
 from bpy.props import StringProperty, BoolProperty
 from ..hubs_component import HubsComponent
 from ..types import Category, PanelType, NodeType
+from ...io.utils import import_component, assign_property
 
 
 class Spawner(HubsComponent):
@@ -42,3 +43,16 @@ class Spawner(HubsComponent):
                 if ob.type == 'ARMATURE':
                     for bone in ob.data.bones:
                         migrate_data(bone)
+
+    @classmethod
+    def gather_import(cls, import_settings, blender_object, component_name, component_value):
+        blender_component = import_component(
+            component_name, blender_object)
+
+        for property_name, property_value in component_value.items():
+            if property_name == 'mediaOptions':
+                setattr(blender_component, "applyGravity",
+                        property_value["applyGravity"])
+            else:
+                assign_property(import_settings, blender_component,
+                                property_name, property_value)
