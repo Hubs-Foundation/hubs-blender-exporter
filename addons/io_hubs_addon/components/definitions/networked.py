9eaf4e9d1d29a7b69958dfec855e2af61086e425
Manuel Martin <manmartin@mozilla.com>
2022-09-20 16:01:53 +0200

    Remove the id property from the networked component and create on export

--- addons/io_hubs_addon/components/definitions/networked.py
+++ addons/io_hubs_addon/components/definitions/networked.py
@@ -4,27 +4,6 @@ from ..types import PanelType, NodeType
 import uuid
 from ..utils import add_component, has_component
 import bpy
-from bpy.app.handlers import persistent
-
-DUPLICATE_OPS = (
-    'OUTLINER_OT_id_paste',
-    'OBJECT_OT_duplicate_move_linked',
-    'OBJECT_OT_duplicate_move',
-    'VIEW3D_OT_pastebuffer'
-)
-
-global operators_length
-
-
-@persistent
-def depsgraph_update(dummy):
-    global operators_length
-    wm = bpy.context.window_manager
-    if wm.operators and len(wm.operators) != operators_length and wm.operators[-1].bl_idname in DUPLICATE_OPS:
-        for ob in bpy.data.objects:
-            if has_component(ob, Networked.get_name()):
-                Networked.init(ob)
-    operators_length = len(wm.operators)
 
 
 class Networked(HubsComponent):
@@ -35,30 +14,10 @@ class Networked(HubsComponent):
         'panel_type': [PanelType.OBJECT, PanelType.BONE]
     }
 
-    id: StringProperty(
-        name="Network ID",
-        description="Network ID"
-    )
-
-    def draw(self, context, layout, panel):
-        layout.label(text="Network ID:")
-        layout.label(text=self.id)
-
-    @classmethod
-    def init(cls, obj):
-        obj.hubs_component_networked.id = str(uuid.uuid4()).upper()
-
-    @staticmethod
-    def register():
-        global operators_length
-        operators_length = 0
-        if not depsgraph_update in bpy.app.handlers.depsgraph_update_post:
-            bpy.app.handlers.depsgraph_update_post.append(depsgraph_update)
-
-    @staticmethod
-    def unregister():
-        if depsgraph_update in bpy.app.handlers.depsgraph_update_post:
-            bpy.app.handlers.depsgraph_update_post.remove(depsgraph_update)
+    def gather(self, export_settings, object):
+        return {
+            'id': str(uuid.uuid4()).upper()
+        }
 
 
 def migrate_networked(component_name):
@@ -66,9 +25,6 @@ def migrate_networked(component_name):
         if component_name in ob.hubs_component_list.items:
             if Networked.get_name() not in ob.hubs_component_list.items:
                 add_component(ob, Networked.get_name())
-
-        if has_component(ob, Networked.get_name()) and not ob.hubs_component_networked.id:
-            Networked.init(ob)
 
     for ob in bpy.data.objects:
         migrate_data(ob)
