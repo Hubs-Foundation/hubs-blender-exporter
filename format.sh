ace9aa811734171d07a40e86db6b5dd4c7105d9e
Manuel Martin <manmartin@mozilla.com>
2022-11-22 14:06:54 +0100

    Add a PEP8 GH and a manual formatter script

---
+++ format.sh
@@ -0,0 +1,8 @@
+if ! command -v autopep8 &> /dev/null
+then
+    echo "Error: autopep8 could not be found: https://pypi.org/project/autopep8/"
+    exit -1
+fi
+
+cd addons
+autopep8 --exclude="**/models" --in-place --recursive .
