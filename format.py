a712ae5098b4ee705074426665807ccfd22e186a
Manuel Martin <manmartin@mozilla.com>
2022-11-23 17:25:24 +0100

    Move format script to python

---
+++ format.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+import os
+from shutil import which
+
+if which("autopep8") is None:
+    print("Error: autopep8 could not be found. Please install autopep8 and run this script again: https://pypi.org/project/autopep8/")
+    sys.exit(1)
+
+cp = subprocess.run(["autopep8", "--exclude=models", "--in-place",
+                     "--recursive", "--max-line-length=120", "--experimental", "addons"])
+sys.exit(cp.returncode)
--- format.sh
+++
@@ -1,8 +0,0 @@
-if ! command -v autopep8 &> /dev/null
-then
-    echo "Error: autopep8 could not be found: https://pypi.org/project/autopep8/"
-    exit -1
-fi
-
-cd addons
-autopep8 --exclude="**/models" --in-place --recursive --max-line-length=120 --experimental .
