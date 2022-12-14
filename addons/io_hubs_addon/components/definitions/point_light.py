6e0884d2ea2ac8f404b18a0089c040247df4d821
Theanine3D <88953117+theanine3D@users.noreply.github.com>
2022-12-13 16:14:34 -0800

    Update point_light.py

--- addons/io_hubs_addon/components/definitions/point_light.py
+++ addons/io_hubs_addon/components/definitions/point_light.py
@@ -1,3 +1,5 @@
+from ..models import pointlight
+from ..gizmos import CustomModelGizmo, bone_matrix_world
 from bpy.props import FloatVectorProperty, FloatProperty, BoolProperty, IntVectorProperty
 from ..hubs_component import HubsComponent
 from ..types import Category, PanelType, NodeType
@@ -49,3 +51,30 @@ class PointLight(HubsComponent):
     shadowRadius: FloatProperty(name="Shadow Radius",
                                 description="Shadow Radius",
                                 default=1.0)
+
+    @classmethod
+    def update_gizmo(cls, ob, bone, target, gizmo):
+        if bone:
+            mat = bone_matrix_world(ob, bone)
+        else:
+            mat = ob.matrix_world.copy()
+
+        gizmo.hide = not ob.visible_get()
+        gizmo.matrix_basis = mat
+
+    @classmethod
+    def create_gizmo(cls, ob, gizmo_group):
+        gizmo = gizmo_group.gizmos.new(CustomModelGizmo.bl_idname)
+        gizmo.object = ob
+        setattr(gizmo, "hubs_gizmo_shape", pointlight.SHAPE)
+        gizmo.setup()
+        gizmo.use_draw_scale = False
+        gizmo.use_draw_modal = False
+        gizmo.color = (0.8, 0.8, 0.8)
+        gizmo.alpha = 0.5
+        gizmo.scale_basis = 1.0
+        gizmo.hide_select = True
+        gizmo.color_highlight = (0.8, 0.8, 0.8)
+        gizmo.alpha_highlight = 1.0
+
+        return gizmo
