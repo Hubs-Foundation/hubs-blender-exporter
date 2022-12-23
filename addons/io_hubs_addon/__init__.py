1c75cad7a151c9b85663f47c30000b2a9a3297a9
Manuel Martin <manmartin@mozilla.com>
2022-12-23 16:11:43 +0100

    Revert "Add support for script reload"
    This reverts commit 586ee7a36a464b7cf51cdd5df046c3969807e0be.

--- addons/io_hubs_addon/__init__.py
+++ addons/io_hubs_addon/__init__.py
@@ -17,27 +17,6 @@ bl_info = {
 }
 
 
-def reload_package(module_dict_main):
-    import importlib
-    from pathlib import Path
-
-    def reload_package_recursive(current_dir, module_dict):
-        for path in current_dir.iterdir():
-            if "__init__" in str(path) or path.stem not in module_dict:
-                continue
-
-            if path.is_file() and path.suffix == ".py":
-                importlib.reload(module_dict[path.stem])
-            elif path.is_dir():
-                reload_package_recursive(path, module_dict[path.stem].__dict__)
-
-    reload_package_recursive(Path(__file__).parent, module_dict_main)
-
-
-if "bpy" in locals():
-    reload_package(locals())
-
-
 def register():
     preferences.register()
     nodes.register()
