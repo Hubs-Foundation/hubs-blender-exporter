d054e5f9a1defd63db0949e9d71ce3f71e7683c6
Manuel Martin <manmartin@mozilla.com>
2023-02-06 13:36:52 +0100

    Update particles test

--- tests/test/test_export.js
+++ tests/test/test_export.js
@@ -331,34 +331,34 @@ describe('Exporter', function () {
         const ext = node.extensions['MOZ_hubs_components'];
         assert.deepStrictEqual(ext, {
           "particle-emitter": {
-            "src": "",
-            "startColor": "#0cff00",
-            "middleColor": "#0cff00",
-            "endColor": "#0cff00",
-            "startOpacity": 1,
-            "middleOpacity": 1,
-            "endOpacity": 1,
-            "sizeCurve": "linear",
-            "colorCurve": "linear",
-            "startSize": 1,
-            "endSize": 1,
-            "sizeRandomness": 0,
-            "ageRandomness": 0,
-            "lifetime": 1,
-            "lifetimeRandomness": 0,
-            "particleCount": 10,
-            "startVelocity": {
-              "x": 0,
-              "y": 0,
-              "z": 0
+            "particleCount":10,
+            "src":"",
+            "ageRandomness":10,
+            "lifetime":1,
+            "lifetimeRandomness":5,
+            "sizeCurve":"linear",
+            "startSize":1,
+            "endSize":1,
+            "sizeRandomness":0,
+            "colorCurve":"linear",
+            "startColor":"#0cff00",
+            "startOpacity":1,
+            "middleColor":"#0cff00",
+            "middleOpacity":1,
+            "endColor":"#0cff00",
+            "endOpacity":1,
+            "velocityCurve":"linear",
+            "startVelocity":{
+              "x":0,
+              "y":0.5,
+              "z":0
             },
-            "endVelocity": {
-              "x": 0,
-              "y": 0,
-              "z": 0
+            "endVelocity":{
+              "x":0,
+              "y":0.5,
+              "z":0
             },
-            "velocityCurve": "linear",
-            "angularVelocity": 0
+            "angularVelocity":0
           }
         });
       });
