bd065c620c79e5b48f0c8b45b73a5e8754a8023c
Manuel Martin <manmartin@mozilla.com>
2023-02-06 14:17:48 +0100

    Remove duplicated particle-emitter test

--- tests/test/test_export.js
+++ tests/test/test_export.js
@@ -363,51 +363,6 @@ describe('Exporter', function () {
         });
       });
 
-      it('can export particle-emitter', function () {
-        let gltfPath = path.resolve(outDirPath, 'particle-emitter.gltf');
-        const asset = JSON.parse(fs.readFileSync(gltfPath));
-
-        assert.strictEqual(asset.extensionsUsed.includes('MOZ_hubs_components'), true);
-        assert.strictEqual(utils.checkExtensionAdded(asset, 'MOZ_hubs_components'), true);
-
-        const node = asset.nodes[0];
-        assert.strictEqual(utils.checkExtensionAdded(node, 'MOZ_hubs_components'), true);
-
-        const ext = node.extensions['MOZ_hubs_components'];
-        assert.deepStrictEqual(ext, {
-          "particle-emitter": {
-            "src": "",
-            "startColor": "#0cff00",
-            "middleColor": "#0cff00",
-            "endColor": "#0cff00",
-            "startOpacity": 1,
-            "middleOpacity": 1,
-            "endOpacity": 1,
-            "sizeCurve": "linear",
-            "colorCurve": "linear",
-            "startSize": 1,
-            "endSize": 1,
-            "sizeRandomness": 0,
-            "ageRandomness": 0,
-            "lifetime": 1,
-            "lifetimeRandomness": 0,
-            "particleCount": 10,
-            "startVelocity": {
-              "x": 0,
-              "y": 0,
-              "z": 0
-            },
-            "endVelocity": {
-              "x": 0,
-              "y": 0,
-              "z": 0
-            },
-            "velocityCurve": "linear",
-            "angularVelocity": 0
-          }
-        });
-      });
-
       it('can export waypoint', function () {
         let gltfPath = path.resolve(outDirPath, 'waypoint.gltf');
         const asset = JSON.parse(fs.readFileSync(gltfPath));
