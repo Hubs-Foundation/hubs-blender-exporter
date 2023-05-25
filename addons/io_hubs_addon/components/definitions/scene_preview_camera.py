0f791c91cbc3b9259f704acc309ea87808ef9059
Manuel Martin <manmartin@mozilla.com>
2023-05-25 19:35:45 +0200

    Added a render operator to take a preview render

--- addons/io_hubs_addon/components/definitions/scene_preview_camera.py
+++ addons/io_hubs_addon/components/definitions/scene_preview_camera.py
@@ -4,7 +4,67 @@ from ..gizmos import CustomModelGizmo
 from ..models import scene_preview_camera
 from mathutils import Matrix
 from math import radians
+from bpy.types import Operator
+from bpy.props import FloatProperty
+import bpy
+from ...utils import rgetattr, rsetattr
 
+def render_post(scene, depsgraph):
+    bpy.context.scene.collection.objects.unlink(camera_object)
+    bpy.data.cameras.remove(camera_data)
+
+    for prop in saved_props:
+        rsetattr(bpy.context, prop, saved_props[prop])
+
+    bpy.app.handlers.render_cancel.remove(render_post)
+    bpy.app.handlers.render_complete.remove(render_post)
+
+class RenderOperator(Operator):
+    bl_idname = "render.hubs_render"
+    bl_label = "Hubs Render"
+    bl_options = { "REGISTER" }
+
+    fov: FloatProperty(name="FOV", min=0, max=radians(180), default=radians(80), subtype="ANGLE", unit="ROTATION")
+
+    def execute(self, context):
+        bpy.app.handlers.render_complete.append(render_post)
+        bpy.app.handlers.render_cancel.append(render_post)
+
+        global camera_data
+        camera_data = bpy.data.cameras.new(name="Temp Hubs Camera Data")
+        camera_data.type = "PERSP"
+        camera_data.clip_start = 0.1
+        camera_data.clip_end = 2000
+        camera_data.lens_unit = "FOV"
+        camera_data.angle = self.fov
+
+        global camera_object
+        camera_object = bpy.data.objects.new("Temp hubs Camera Object", camera_data)
+        context.scene.collection.objects.link(camera_object)
+        camera_object.matrix_world = context.active_object.matrix_world.copy()
+        rot_offset = Matrix.Rotation(radians(90), 4, 'X')
+        camera_object.matrix_world = camera_object.matrix_world @ rot_offset
+
+        overrides = [
+            ("preferences.view.render_display_type", "NONE"),
+            ("scene.camera", camera_object),
+            ("scene.render.resolution_x", 1920),
+            ("scene.render.resolution_y", 1080),
+            ("scene.render.resolution_percentage", 100),
+            ("scene.render.image_settings.file_format", "PNG"),
+            ("scene.render.filepath", f"{context.scene.render.filepath}/scene-preview-camera.png"),
+        ]
+
+        global saved_props
+        saved_props = {}
+        for (prop, value) in overrides:
+            if prop not in saved_props:
+                saved_props[prop] = rgetattr(bpy.context, prop)
+            rsetattr(bpy.context, prop, value)
+
+        bpy.ops.render.render("INVOKE_DEFAULT", write_still=True)
+
+        return {'FINISHED'}
 
 class ScenePreviewCamera(HubsComponent):
     _definition = {
@@ -16,6 +76,8 @@ class ScenePreviewCamera(HubsComponent):
         'icon': 'CAMERA_DATA',
         'version': (1, 0, 0)
     }
+
+    fov: FloatProperty(name="FOV", min=0, max=radians(180), default=radians(80), subtype="ANGLE", unit="ROTATION")
 
     def pre_export(self, export_settings, host, ob=None):
         global backup_name
@@ -50,3 +112,18 @@ class ScenePreviewCamera(HubsComponent):
         gizmo.alpha_highlight = 1.0
 
         return gizmo
+    
+    def draw(self, context, layout, panel):
+        row = layout.row()
+        row.prop(data=self, property="fov")
+        row = layout.row()
+        op = row.operator("render.hubs_render", text="Render Preview Camera")
+        op.fov = self.fov
+    
+    @ staticmethod
+    def register():
+        bpy.utils.register_class(RenderOperator)
+
+    @ staticmethod
+    def unregister():
+        bpy.utils.unregister_class(RenderOperator)
